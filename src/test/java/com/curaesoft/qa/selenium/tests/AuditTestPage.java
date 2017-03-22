1eaf298ac7760f7873be6c5dfc93ee8cc04b2a28
nikko gerald eroy <nikkogerald.eroy@gmail.com>
2017-03-23 05:07:37 +0800

    update for  order audit and note audit

--- src/test/java/com/curaesoft/qa/selenium/tests/AuditTestPage.java
+++ src/test/java/com/curaesoft/qa/selenium/tests/AuditTestPage.java
@@ -1,19 +1,19 @@
-package tests;
+package com.curaesoft.qa.selenium.tests;
 
+import com.curaesoft.qa.selenium.CommonPages.HomePage;
 import com.curaesoft.qa.selenium.Config.Constant;
+import com.curaesoft.qa.selenium.base.BaseWebDriver;
+import com.curaesoft.qa.selenium.utilities.ExcelUtils;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
+import org
+        .testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.curaesoft.qa.selenium.CommonPages.HomePage;
-import com.curaesoft.qa.selenium.base.BaseWebDriver;
-import com.curaesoft.qa.selenium.utilities.ExcelUtils;
-
 /**
- * Created by SE on 2/22/2017.
+ * Created by SE on 3/15/2017.
  */
-public class ProfilePageTest extends BaseWebDriver {
+public class AuditTestPage  extends BaseWebDriver {
     HomePage homePage;
     Boolean result;
     Boolean success = true;
@@ -22,38 +22,48 @@ public class ProfilePageTest extends BaseWebDriver {
     public void login() {
 
         try {
-            homePage = this.loginPage.login("qa");
+            homePage = this.loginPage.login("Auditor");
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Failed to login into the application !");
         }
     }
 
-    @Test (priority=15)
-    public void UpdateSettings() {
+    //@Test
+
+    public void NoteApproval() {
         try {
             ExcelUtils eu = new ExcelUtils();
-            eu.execute(this.driver, "profile.xlsx");
+            eu.execute(this.driver, "NoteAudit.xlsx");
         } catch (Exception e) {
-            success = false;
+            success = true;
             e.printStackTrace();
             System.out.println("Failed to create patient.");
         }
-
-
     }
-
+    @Test(priority=6)
+    public void OrderNotes() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.execute(this.driver, "OrderAudit.xlsx");
+        } catch (Exception e) {
+            success = true;
+            e.printStackTrace();
+            System.out.println("Failed to create patient.");
+        }
+    }
 
     @AfterMethod
     public void logout() {
         try {
-            if(Constant.Debugging == false ){
+            if(Constant.Debugging == true ){
                 if(success){
                     result = this.loginPage.logout();
                     Assert.assertTrue(result);
                 }
             }
         } catch (Exception e) {
+            success = false;
             e.printStackTrace();
             System.out.println("Failed to login into the application");
         }
