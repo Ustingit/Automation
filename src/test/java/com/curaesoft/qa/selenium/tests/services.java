3af14cee3236543eb1bf0e92ff285b81d4aa7d97
Yvanne Espina <yvanne.espina@cvhcare.com>
2017-06-20 02:06:25 +0800

    fix

--- src/test/java/com/curaesoft/qa/selenium/tests/services.java
+++ src/test/java/com/curaesoft/qa/selenium/tests/services.java
@@ -353,7 +353,7 @@ public class services {
     }
 	
 	@Test(priority = 10)
-	public void Clinician_Audti() {
+	public void Clinician_Audit() {
         try {
         	click("//div[@layout='column']/button[@aria-label='Clinician Portal']");
         	visible("//span/span[@class='ng-scope ng-binding' and contains(text(),'Clinician')]");
