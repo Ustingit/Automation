f3c4a5379648c025e7e8e76d7689ebc52e4b6b60
Harold Gordo <harold.gordo@cvhcare.com>
2017-06-28 06:57:24 +0800

    Update

---
+++ src/test/java/com/curaesoft/qa/selenium/tests/EditPageTest.java
@@ -0,0 +1,146 @@
+package tests;
+
+import com.curaesoft.qa.selenium.CommonPages.HomePage;
+import com.curaesoft.qa.selenium.Config.Constant;
+import com.curaesoft.qa.selenium.base.BaseWebDriver;
+import com.curaesoft.qa.selenium.utilities.ExcelUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by SE on 2/17/2017.
+ */
+public class EditPageTest  extends BaseWebDriver {
+    HomePage homePage;
+    Boolean result;
+    Boolean success = true;
+    @BeforeMethod
+    public void login() {
+        try {
+            homePage = this.loginPage.login("qa");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to login into the application !");
+        }
+    }
+
+
+    @Test(priority=85 , groups = {"patientdetails"})
+
+    public void EditPatient() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.account();
+            eu.execute(this.driver, "EditName.xlsx","EditPatient");
+        } catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to create patient.");
+        }
+
+    }
+    @Test(priority=86 , groups = {"patientdetails"})
+
+    public void Scheduling() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.account();
+            eu.execute(this.driver, "EditName.xlsx","Scheduler");
+        } catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to create patient.");
+        }
+
+    }
+    @Test(priority=87 , groups = {"patientdetails"})
+
+    public void AuditingNote() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.account();
+            eu.execute(this.driver, "EditName.xlsx","AuditingNote");
+        } catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to create patient.");
+        }
+
+    }
+
+    @Test(priority=88 , groups = {"patientdetails"})
+
+    public void AuditingOrder() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.account();
+            eu.execute(this.driver, "EditName.xlsx","AuditingOrder");
+        } catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to create patient.");
+        }
+
+    }
+
+
+    @Test(priority=89 , groups = {"patientdetails"})
+
+    public void Authorization() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.account();
+            eu.execute(this.driver, "EditName.xlsx","Authorization");
+        } catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to create patient.");
+        }
+
+    }
+
+
+    @Test(priority=90 , groups = {"patientdetails"})
+
+    public void CQ() {
+        try {
+            ExcelUtils eu = new ExcelUtils();
+            eu.account();
+            eu.execute(this.driver, "EditName.xlsx","CQ");
+        } catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+            Assert.fail(e.toString());
+            System.out.println("Failed to create patient.");
+        }
+
+    }
+
+    @AfterMethod
+    public void logout() {
+        try {
+            if(Constant.Debugging == false){
+                result = this.loginPage.logout();
+                Assert.assertTrue(result);
+            }else{
+                if(Constant.Success){
+                    result = this.loginPage.logout();
+                    Assert.assertTrue(result);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to login into the application");
+            Assert.fail(e.toString());
+        }
+    }
+}
