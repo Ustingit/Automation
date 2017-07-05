093dd10da467ff2e2d73556b606852abe3fb16d6
Yvanne Espina <yvanne.espina@cvhcare.com>
2017-07-06 06:44:09 +0800

    fix

--- src/main/java/com/curaesoft/qa/selenium/utilities/ExcelUtils.java
+++ src/main/java/com/curaesoft/qa/selenium/utilities/ExcelUtils.java
@@ -209,7 +209,7 @@ public class ExcelUtils {
 				if(error_notice.equals("") || Constant.ErrorNotice == true){
 					Assert.fail("Fail to locate xpath on row number "+(rownum+1)+" in "+ srcfile +"("+ sheetn+")"+"\n\n"+e);
 				}else{
-					Assert.fail(srcfile +"("+ sheetn+") "+(rownum+1)+":"+error_notice);
+					Assert.fail(srcfile +"("+ sheetn+") "+(rownum+1)+":"+error_notice+"\n\n"+e);
 				}
 
 				throw e;
