722a8359d6b65c0d2db15373b2f7916539cc536d
Admin77-PC\Admin 77 <castrovalley2017>
2020-04-24 07:44:02 +0800

    update

--- src/main/java/com/curaesoft/qa/selenium/Config/Constant.java
+++ src/main/java/com/curaesoft/qa/selenium/Config/Constant.java
@@ -22,14 +22,13 @@ public class Constant {
 //	public static final String webURL = "https://june.curaesoft.com";
 	//public static final String webURL = "http://35.232.210.17:9999/#/login";
 	//public static final String webURL = "http://34.68.75.255:9999/";
-	public static final String webURL = "http://35.232.210.17:9999/#/login";
+	public static final String webURL = "https://34.107.155.61";
 
 
 	public static final String browserType = "*chrome";
 	public static final String File_TestData = "resources/Testdata.xlsx";
 	public static final String excelSheetName = "FirstSet";
-	// Roles
-	public static final String QA_ROLE = "qa";
+	// Roles	public static final String QA_ROLE = "qa";
 	public static final String CLINICIAN_ROLE = "clinician";
 	public static final String MANAGER_ROLE = "manager";
 	public static final String INTAKE_ROLE = "Intake";
