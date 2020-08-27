09a5722ba5e4daf63adf619c933556a1c1dc824e
Admin77-PC\Admin 77 <castrovalley2017>
2020-08-27 22:03:03 +0800

    updated

--- src/main/java/com/curaesoft/qa/selenium/Config/Constant.java
+++ src/main/java/com/curaesoft/qa/selenium/Config/Constant.java
@@ -22,8 +22,8 @@ public class Constant {
 //	public static final String webURL = "https://june.curaesoft.com";
 	//public static final String webURL = "http://35.232.210.17:9999/#/login";
 	//public static final String webURL = "http://34.68.75.255:9999/";
-	public static final String webURL = "https://34.107.155.61";
-
+//	public static final String webURL = "https://34.107.155.61";
+	public static final String webURL = "https://test.curaesoft.com/";
 
 	public static final String browserType = "*chrome";
 	public static final String File_TestData = "resources/Testdata.xlsx";
